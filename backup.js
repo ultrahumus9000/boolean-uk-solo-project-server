adeca7ae5485b507d02cc92730008c3f09b31a76
Linlin Li <81304801+ultrahumus9000@users.noreply.github.com>
2022-01-27 20:10:49 +0000

    backup

---
+++ backup.js
@@ -0,0 +1,15 @@
+NODE_ENV = "development";
+PORT = 4000;
+
+FRONTEND_URL = "http://localhost:3000";
+
+DATABASE_URL =
+  "postgres://xgfdjjbs:IOcKmUIEoj6toxItww4BC9RjI3aQiOML@hattie.db.elephantsql.com/xgfdjjbs?schema=cinema";
+SHADOW_DATABASE_URL =
+  "postgres://fwztkihc:bIjq9u0eTlYDCXdJ_qCXSceu20knbyuP@tai.db.elephantsql.com/fwztkihc?schema=cinema";
+
+JWT = "supersecret";
+STRIPE_SECRET_KEY =
+  sk_test_51JaJ4QKfgna7lIMqof0on9c0XusLcw685LKqExZZ5kIrhqFwDXWbWUkIwH1og3HllsusEKW8dOrYj2HmqCRu1BKo005AqTh9Mf;
+STRIPE_PUBLIC_KEY =
+  pk_test_51JaJ4QKfgna7lIMqMHl7lY2IL0SghVmZarqKFt6YsYGiswYxpefEzRrXTvJMF28TEqlqYN3zLgiyPO5Z6SnVZT1t00zwqaHsrB;
